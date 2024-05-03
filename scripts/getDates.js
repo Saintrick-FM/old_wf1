44077bbcb8566a22eedc3b2c6fcd52d76733f98e
Francy-malo <francy.malonga@nanocreatives.com>
2024-05-03 19:23:02 +0100

    almost done with first assignment

---
+++ scripts/getDates.js
@@ -0,0 +1,9 @@
+// getDates.js
+
+// Dynamically update copyright year
+let year = document.getElementById("year");
+year.textContent = new Date().getFullYear();
+
+// Get last modified date of the document
+document.getElementById("last-modified").textContent =
+  "Last modified: " + document.lastModified;
